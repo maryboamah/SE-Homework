e42c02ff23570a5b0d4488ce725f8a3f4352f8de
maryboamah <maryboamah680@gmail.com>
2021-12-03 13:55:17 -0500

    adding node module and package

--- Week2_HW1_Strings/Exercise1-Palindrome.js
+++ Week2_HW1_Strings/Exercise1-Palindrome.js
@@ -2,17 +2,18 @@
 let prompt = require('prompt-sync')()
 let str = prompt("Enter a anything ")
 
-//reverse the string and store it in a variable called strReverse
+//create a variable to hold reverse string starting with an empty string
 let strReverse = "";
+
 //get the length of the str
 let len = str.length
 
 for(let i= len; i>=0; i--){
-
+//reverse the string and store in variable strReverse
     strReverse+=str.charAt(i)
 
 }
-console.log(strReverse)
+
 
 if (strReverse === str) {
     console.log("This is a palindrome")
