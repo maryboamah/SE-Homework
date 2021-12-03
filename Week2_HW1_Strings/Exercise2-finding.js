8bbc352b6834151b2065b4203c9f09c41e576eb2
maryboamah <maryboamah680@gmail.com>
2021-12-03 14:20:58 -0500

    adding changes to exercise two

---
+++ Week2_HW1_Strings/Exercise2-finding.js
@@ -0,0 +1,22 @@
+//take  user input
+//loop through user input
+//check if each letter in the word or sentence is letters, numbers or 
+//check the string for letters a-z
+//then store in a variable called my letters
+//check for numbers 0-9
+//then store it in variable called myNumbers
+//check for other special characters
+//store in a variable called mySpecialChar
+
+//create a prompt to get 
+let prompt = require('prompt-sync')()
+let str = prompt("Enter a any Sentence or word: ")
+
+//create variable to store letters
+let myLetters = "";
+
+//create variable to store numbers
+let myNumbers = "";
+
+//create a variable to add all special characters
+let mySpecialChar = "";
