bb46161d50190b5825f2b22c2523c46a9d660fe2
maryboamah <maryboamah680@gmail.com>
2021-12-03 16:57:52 -0500

    clearing pseudo code

--- Week2_HW1_Strings/Exercise2-finding.js
+++ Week2_HW1_Strings/Exercise2-finding.js
@@ -1,13 +1,3 @@
-//take  user input
-//loop through user input
-//check if each letter in the word or sentence is letters, numbers or 
-//check the string for letters a-z
-//then store in a variable called my letters
-//check for numbers 0-9
-//then store it in variable called myNumbers
-//check for other special characters
-//store in a variable called mySpecialChar
-
 //create a prompt to get 
 let prompt = require('prompt-sync')()
 let str = prompt("Enter a any Sentence or word: ")
