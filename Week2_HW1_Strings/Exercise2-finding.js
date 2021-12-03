301d3f574eac7c94a9bd0907148098ea183a419c
maryboamah <maryboamah680@gmail.com>
2021-12-03 14:58:55 -0500

    adding to exercise 2

--- Week2_HW1_Strings/Exercise2-finding.js
+++ Week2_HW1_Strings/Exercise2-finding.js
@@ -12,6 +12,8 @@
 let prompt = require('prompt-sync')()
 let str = prompt("Enter a any Sentence or word: ")
 
+//getting the length of the string
+let len = str.length
 //create variable to store letters
 let myLetters = "";
 
@@ -20,3 +22,14 @@ let myNumbers = "";
 
 //create a variable to add all special characters
 let mySpecialChar = "";
+
+//using loop to get the individual characters in a string
+for(let i = 0; i<=len; i++){
+   
+ if(str.charAt(i)== 1 || str.charAt(i)== 2||str.charAt(i)== 3 ||str.charAt(i)== 4 ||str.charAt(i)== 5||str.charAt(i)== 6||str.charAt(i)== 7||str.charAt(i)== 8||str.charAt(i)== 9||str.charAt(i)== 0){
+myNumbers+=str.charAt(i) + "";
+ }
+
+}
+//this is to print out all the numbers in the code
+console.log(`These are the numbers in your word or sentenec: ${myNumbers.toString()}`)
