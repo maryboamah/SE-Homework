df7e7b1898ad044761c1ebb163d5978d03a3ab62
maryboamah <maryboamah680@gmail.com>
2021-12-05 18:54:31 -0500

    adding all the changes

--- Week2_HW1_Strings/ExerciseBonus.js
+++ Week2_HW1_Strings/ExerciseBonus.js
@@ -20,3 +20,25 @@ console.log(`This capitilizes words using array methods and this is the result $
 
 
 //Using just strings to achieve th esame thing
+userWord = userWord.replace(userWord.charAt(0), userWord.charAt(0).toUpperCase())
+let userWordCapital =""
+
+
+for(let i=0; i<userWord.length; i++ ){
+        //only find index of spaces
+        if(userWord.charAt(i) === " "){
+            // make userCapital  this empty on each loop
+            userWordCapital = ""
+            //replace the word after space with capital
+            userWordCapital = userWord.replace(userWord.charAt(i+1), userWord.charAt(i+1).toUpperCase())
+          //console.log(userWord.charAt(i+1))
+          
+          //store that word in userWord
+            userWord = userWordCapital
+          //console.log(i)
+        }
+        
+    
+    }
+
+    console.log(`This is the result from just using string methods  ${userWordCapital}`)
