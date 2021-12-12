08bf993a5824266bb0d0287c18a73017681fc674
maryboamah <maryboamah680@gmail.com>
2021-12-11 23:21:56 -0500

    Adding degree coversio

--- Week3_HW2_Functions/celsiusToFahrenheit.js
+++ Week3_HW2_Functions/celsiusToFahrenheit.js
@@ -2,5 +2,12 @@ function celsiusToFahrenheit(){
     let celsius = document.getElementById("celsius").value
     let fahrenheit = (celsius * 9/5) + 32
     console.log(celsius)
-    return document.getElementById("fahrenheit").innerHTML = `${celsius} degree celsius coverted to Fahrenheit is <span style="color: Blue"> ${fahrenheit}</span`
+    return document.getElementById("fahrenheit").innerHTML = `${celsius}&#8451; coverted to Fahrenheit is <span style="color: Blue"> ${fahrenheit}&#8457;</span`
+}
+
+function fahrenheitToCelsius(){
+    let fahrenheit = document.getElementById("fahrenheitdegree").value
+    let celsius = ((fahrenheit -32) *5/9).toFixed(2)
+    console.log(celsius)
+    return document.getElementById("celsiusdegree").innerHTML = `${fahrenheit}&#8457;  coverted to celsius is <span style="color: Blue"> ${celsius}&#8451;</span`
 }
